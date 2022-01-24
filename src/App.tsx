d5f3c25715e399aec4a3eb1d8b5746ae3969f273
dm1tro69 <dimolg22@gmail.com>
2022-01-24 13:35:17 +0200

    add service page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from "./home/Home";
 import About from "./about/About";
+import Service from "./service/Service";
 
 const App = () => {
   return (
     <div className={'App'}>
       <Home/>
         <About/>
+        <Service/>
     </div>
   );
 }
