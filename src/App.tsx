022da088df6b057ce7e4e2211fb7640a675ce7c6
dm1tro69 <dimolg22@gmail.com>
2022-01-25 13:04:01 +0200

    add blog page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css';
 import Home from "./home/Home";
 import About from "./about/About";
 import Service from "./service/Service";
+import Project from "./project/Project";
+import Blog from "./blog/Blog";
+import Contact from "./contact/Contact";
 
 const App = () => {
   return (
@@ -10,6 +13,9 @@ const App = () => {
       <Home/>
         <About/>
         <Service/>
+      <Project/>
+        <Blog/>
+        <Contact/>
     </div>
   );
 }
